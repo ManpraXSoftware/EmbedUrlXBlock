c040e67ed0f7b35d90b5d33d804c675ec6869cf0
Ubuntu <ubuntu@ip-172-31-2-251.ap-south-1.compute.internal>
2024-03-26 12:08:37 +0000

    Update new windows flag in student view url

--- embedurl/embedurl.py
+++ embedurl/embedurl.py
@@ -121,6 +121,7 @@ class embedurlXBlock(XBlock):
         Make sure to include `student_view_data=scorm` to URL params in the request.
         """
         return {'last_modified': self.new_window_button,
+                'new_window': self.new_window_button,
                 'scorm_data': self.url
         }
 	
